5d1d727be68552ebb5cfafc79138acca12ca3784
francisbagsic12 <hugobayani@gmail.com>
2026-01-24 14:53:12 +0800

    Update CORS origin to include Netlify app

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const io = socketIo(server, { cors: { origin: "*" } });
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://ai-driven-qne.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   }),
