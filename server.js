de70accc7e5164023c8b90eedbcba5f783d7edd3
francisbagsic12 <hugobayani@gmail.com>
2026-04-20 10:14:54 +0800

    Update CORS origin for deployment

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const isValidObjectId = (id) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://ai-drive-qne.netlify.app"],
+    origin: ["http://localhost:5173", "https://ai-disaster.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   }),
